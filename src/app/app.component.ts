1ec0e1761d71d4579f8ac8b7f063b89d167464a0
rizkysyukur <rizkysyukur10@gmail.com>
2023-11-13 11:20:27 +0700

    Fix router link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class AppComponent {
   title = 'sds_system';
